b8f3c37c3f30fbaae43820bbd0df1d5299da73ba
pizza <npwchr.work@gmail.com>
2021-09-15 23:26:59 +0700

    init home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,7 @@ const Home = () => {
   return (
     <div>
       <MainLayout>
-        <div className="flex items-center justify-center">
-          <div className="h-[111px] w-[111px] bg-black"></div>
-        </div>
+        
       </MainLayout>
     </div>
   );
